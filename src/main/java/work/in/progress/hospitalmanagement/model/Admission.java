07d649df5115ef1a0eefcefc1a7e1ca8df030742
jablonskiba <jablonskiba@gmail.com>
2019-04-15 20:23:53 +0200

    Enable auditing for Admission enitities

--- src/main/java/work/in/progress/hospitalmanagement/model/Admission.java
+++ src/main/java/work/in/progress/hospitalmanagement/model/Admission.java
@@ -2,12 +2,14 @@ package work.in.progress.hospitalmanagement.model;
 
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
+@EntityListeners(AuditingEntityListener.class)
 abstract class Admission {
 
     @Id
