7d32d363efb314929156cb5399d61e82ac61e66d
Andrzej Grabowski <andys.grabek@icloud.com>
2019-04-17 23:24:34 +0200

    Add main class documentation, add jfoenix dependency

--- src/main/java/work/in/progress/hospitalmanagement/HospitalManagementApplication.java
+++ src/main/java/work/in/progress/hospitalmanagement/HospitalManagementApplication.java
@@ -9,9 +9,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import work.in.progress.hospitalmanagement.controller.AbstractViewController;
 import work.in.progress.hospitalmanagement.controller.SplashScreenViewController;
 
+
+/**
+ * Main application class, starting both the SpringApplication and JavaFX front end.
+ *
+ * @author Andrzej Grabowski
+ * @author Bartłomiej Jabłoński
+ */
 @SpringBootApplication
 public class HospitalManagementApplication extends Application {
 
+	/**
+	 * Application entry point. Requires no arguments.
+	 * @param args CL arguments list. Unused.
+	 */
 	public static void main(String[] args) {
 		ApplicationContextSingleton.setContext(SpringApplication.run(HospitalManagementApplication.class, args));
 		launch(args);
