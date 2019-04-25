d7eb1e3f101ace50728261ca54d629ba2e792338
Andrzej Grabowski <andys.grabek@icloud.com>
2019-04-25 12:03:05 +0200

    Annotated AdmisstionManagementViewController

--- src/main/java/work/in/progress/hospitalmanagement/controller/AdmissionManagementViewController.java
+++ src/main/java/work/in/progress/hospitalmanagement/controller/AdmissionManagementViewController.java
@@ -2,10 +2,15 @@ package work.in.progress.hospitalmanagement.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+@Component
+@Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
 public class AdmissionManagementViewController extends AbstractViewController {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
