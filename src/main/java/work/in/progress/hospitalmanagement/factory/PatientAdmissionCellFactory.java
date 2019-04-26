14cc5c36f9211c12134078181412cf80b3e2a058
Andrzej Grabowski <andys.grabek@icloud.com>
2019-04-26 11:05:21 +0200

    Add patient admission cell factory

---
+++ src/main/java/work/in/progress/hospitalmanagement/factory/PatientAdmissionCellFactory.java
@@ -0,0 +1,23 @@
+package work.in.progress.hospitalmanagement.factory;
+
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+import work.in.progress.hospitalmanagement.cell.PatientAdmissionCell;
+import work.in.progress.hospitalmanagement.model.Patient;
+
+/**
+ * Custom cell factory class to be used with a {@link ListView} containing {@link Patient} objects.
+ * @author Andrzej Grabowski
+ */
+public class PatientAdmissionCellFactory implements Callback<ListView<Patient>, ListCell<Patient>> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ListCell<Patient> call(ListView<Patient> listView) {
+        return new PatientAdmissionCell();
+    }
+
+}
