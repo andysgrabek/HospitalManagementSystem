6292279aed6e0624402f8575f7b59fefbaea7255
Andrzej Grabowski <andys.grabek@icloud.com>
2019-05-04 23:49:48 +0200

    Update appointment cell test

--- src/test/java/work/in/progress/hospitalmanagement/cell/AppointmentCellTest.java
+++ src/test/java/work/in/progress/hospitalmanagement/cell/AppointmentCellTest.java
@@ -6,6 +6,8 @@ import work.in.progress.hospitalmanagement.model.OutpatientAdmission;
 import work.in.progress.hospitalmanagement.rule.JavaFXThreadingRule;
 import work.in.progress.hospitalmanagement.util.Mocks;
 
+import java.time.format.DateTimeFormatter;
+
 import static org.junit.Assert.*;
 
 public class AppointmentCellTest {
@@ -18,14 +20,12 @@ public class AppointmentCellTest {
         OutpatientAdmission item = Mocks.outpatientAdmission();
         AppointmentCell appointmentCell = new AppointmentCell();
         appointmentCell.updateItem(item, false);
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm");
         assertEquals(item.getPatient().getName()
-                + " " + item.getPatient().getSurname()
-                + " appointment for "
-                + item.getVisitDate().getDayOfMonth()
-                + "-" + item.getVisitDate().getMonthValue()
-                + "-" + item.getVisitDate().getYear()
-                + " at " + item.getVisitDate().getHour() + ":" + item.getVisitDate().getMinute()
-                + " in " + item.getDepartment().getName(),
+                        + " " + item.getPatient().getSurname()
+                        + " appointment for "
+                        + timeFormatter.format(item.getVisitDate())
+                        + " in " + item.getDepartment().getName(),
                 appointmentCell.getLabel().getText());
     }
 
